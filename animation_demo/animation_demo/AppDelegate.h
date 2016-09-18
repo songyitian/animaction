587e843f524e232bf0be1b8c47ef836dc1afa3b8
chuanxiao <chuanxiao@mac2deimac.local>
2016-09-18 15:23:51 +0800

    init demo

---
+++ animation_demo/animation_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  animation_demo
+//
+//  Created by chuanxiao on 16/9/18.
+//  Copyright © 2016年 chuanxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ animation_demo/animation_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  animation_demo
+//
+//  Created by chuanxiao on 16/9/18.
+//  Copyright © 2016年 chuanxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
